b243c55ff541f01918c3175503db29b22c6f0042
Aamir Khan <38729443+Aamirkhan10218@users.noreply.github.com>
2021-03-01 23:10:45 +0500

    Add project files.

---
+++ API/Controllers/MessageController.cs
@@ -0,0 +1,31 @@
+﻿using API.Hubs;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("CORSPolicy")]
+    public class MessageController : ControllerBase
+    {
+        private readonly IHubContext<MessageHub> hubContext;
+
+        public MessageController(IHubContext<MessageHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage([FromQuery] string Message)
+        {
+            await hubContext.Clients.All.SendAsync("message",$"{ Message}");
+            return Ok(new
+            {
+                StatusCode = 200,
+                Message = "Send Successfully."
+            });
+        }
+    }
+}
